2c1da3385b3399207212e8e323497a1db3a3ff9b
Alexis Tio <tioalexis@protonmail.com>
2025-06-26 11:08:32 +0800

    Temporarily commented out alignment logic

--- Assets/Cyl/BubbleShooter/Scripts/Fsm/InitializeGameplaySceneState.cs
+++ Assets/Cyl/BubbleShooter/Scripts/Fsm/InitializeGameplaySceneState.cs
@@ -13,29 +13,29 @@ namespace Cyl.BubbleShooter.Fsm
             Finish();
         }
 
-        private void AlignCameraToGrid()
-        {
-            var gridSize = BubbleGrid.CalculateGridSize();
-            var cameraPosition = Camera.transform.position;
-            cameraPosition.x = (gridSize.x / 2f) - 0.5f; // TODO: remove magic number
-            BubbleShooterGame.Camera.transform.position = cameraPosition;
-        }
-
-        private void AlignWallsToGrid()
-        {
-            var gridSize = BubbleGrid.CalculateGridSize();
-            
-            var ceilingPosition = View.Ceiling.transform.position;
-            ceilingPosition.y = View.TopEdgeAnchor.position.y;
-            View.Ceiling.transform.position = ceilingPosition;
-            
-            var leftWallPosition = View.LeftWall.transform.position;
-            leftWallPosition.x = -BubbleGrid.CellInnerRadius;
-            View.LeftWall.transform.position = leftWallPosition;
-            
-            var rightWallPosition = View.RightWall.transform.position;
-            rightWallPosition.x = gridSize.x - BubbleGrid.CellInnerRadius;
-            View.RightWall.transform.position = rightWallPosition;
-        }
+        // private void AlignCameraToGrid()
+        // {
+        //     var gridSize = BubbleGrid.CalculateGridSize();
+        //     var cameraPosition = Camera.transform.position;
+        //     cameraPosition.x = (gridSize.x / 2f) - 0.5f; // TODO: remove magic number
+        //     BubbleShooterGame.Camera.transform.position = cameraPosition;
+        // }
+        
+        // private void AlignWallsToGrid()
+        // {
+        //     var gridSize = BubbleGrid.CalculateGridSize();
+        //     
+        //     var ceilingPosition = View.Ceiling.transform.position;
+        //     ceilingPosition.y = View.TopEdgeAnchor.position.y;
+        //     View.Ceiling.transform.position = ceilingPosition;
+        //     
+        //     var leftWallPosition = View.LeftWall.transform.position;
+        //     leftWallPosition.x = -BubbleGrid.CellInnerRadius;
+        //     View.LeftWall.transform.position = leftWallPosition;
+        //     
+        //     var rightWallPosition = View.RightWall.transform.position;
+        //     rightWallPosition.x = gridSize.x - BubbleGrid.CellInnerRadius;
+        //     View.RightWall.transform.position = rightWallPosition;
+        // }
     }
 }
